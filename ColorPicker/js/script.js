03b552455f7ac230877396d99b1ec0e73b8fc401
tatsuya <clarkmaster@icloud.com>
2024-01-07 19:26:11 +0900

    if文を追加してwhiteとblackを表示！

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -6,7 +6,13 @@ const colorBg = () => {
   //選択した色を背景色に設定
   document.body.style.backgroundColor = color.value;
   // カラーコードを表示
-  text.textContent = `カラーコード : ${color.value}`;
+  if (color.value === '#ffffff') {
+    text.textContent = `カラーコード : ${color.value} (white)`;
+  } else if (color.value === '#000000') {
+    text.textContent = `カラーコード : ${color.value} (black)`;
+  } else {
+    text.textContent = `カラーコード : ${color.value}`;
+  }
 }
 
 //カラーピッカーが変更されたら colorBg を発動させる
