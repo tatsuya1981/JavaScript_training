eee88bf18f9e7963c4227345fe6fb3388f006a76
tatsuya <clarkmaster@icloud.com>
2024-01-07 19:01:42 +0900

    カラーピッカーの背景色に変化機能追加！

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,2 +1,13 @@
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent= 'カラーコード:';
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+// カラーピッカーを操作した時の一連の動作
+const colorBg = () => {
+  //選択した色を背景色に設定
+  document.body.style.backgroundColor = color.value;
+  // カラーコードを表示
+  text.textContent = `カラーコード : ${color.value}`;
+}
+
+//カラーピッカーが変更されたら colorBg を発動させる
+color.addEventListener('input', colorBg);
