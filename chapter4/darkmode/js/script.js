bb4a29563e4061b51dac5e5cbbe9641f68777cb4
tatsuya <clarkmaster@icloud.com>
2024-01-13 14:40:02 +0900

    darkmode

--- chapter4/darkmode/js/script.js
+++ chapter4/darkmode/js/script.js
@@ -1,5 +1,11 @@
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () => {
-  document.body.classList.toggle
-})
+  document.body.classList.toggle('dark-theme');
+
+if (btn.textContent === 'ダークモードにする'){
+  btn.textContent = 'ライトモードにする';
+} else {
+    btn.textContent = 'ダークモードにする';
+  }
+});
