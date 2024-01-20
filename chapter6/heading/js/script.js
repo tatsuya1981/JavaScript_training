b8cd36d4acf5c43fcf7a49543ffe2e97acae25a9
tatsuya <clarkmaster@icloud.com>
2024-01-20 20:57:46 +0900

    うねうね動く背景！

--- chapter6/heading/js/script.js
+++ chapter6/heading/js/script.js
@@ -1,12 +1,17 @@
 const heading = document.querySelector('#heading');
 
 const keyframes = {
-  opacity: [0, 1],
-  translate: ['0 50px', 0],
+  borderRadius: [
+    '20% 50% 50% 70%/50% 50% 70% 50%',
+    '50% 20% 50% 50%/40% 40% 60%,60%',
+    '50% 40% 20% 40%/40% 50% 50% 80%',
+    '50% 50% 50% 20%/40% 40% 60% 60%',
+  ],
 };
 const options = {
-  duration: 2000,
-  easing: 'ease',
+  duration: 8000,
+  direction: 'alternate',
+  iterations: Infinity,
 };
 
 heading.animate(keyframes, options);
