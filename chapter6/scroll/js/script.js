2535f720d8b9b7a844e643fbbbaf472d581f6ced
tatsuya <clarkmaster@icloud.com>
2024-01-21 12:14:46 +0900

    修正！

--- chapter6/scroll/js/script.js
+++ chapter6/scroll/js/script.js
@@ -7,12 +7,8 @@ const showkirin = (entries) => {
   entries[0].target.animate(keyframes, 600);
 };
 
-const options = {
-  rootMargin: '-100px',
-  threshold: [0, 0.2, 0.5, 0.8, 1],
-};
 // 監視ロボットに名前を付けて導入
-const kirinObserver = new IntersectionObserver(showkirin, options);
+const kirinObserver = new IntersectionObserver(showkirin);
 
 // kirinを監視する様に指示
 const kirin = document.querySelector('#kirin');
