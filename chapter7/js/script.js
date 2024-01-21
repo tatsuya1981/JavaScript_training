0c3da40790f647c122435497ebeca78cf3982d02
tatsuya <clarkmaster@icloud.com>
2024-01-21 17:13:53 +0900

    サムネイル画像表示！

--- chapter7/js/script.js
+++ chapter7/js/script.js
@@ -52,3 +52,22 @@ window.addEventListener('load', () => {
     }
   );
 });
+
+/*
+画像ギャラリー
+================================================ */
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+
+// for(let i = 0; i < thumbImages.length; i++) {
+//   thumbImages[i].addEventListener('mouseover', (event) => {
+//     mainImage.src = event.target.src;
+//     mainImage.animate({opacity: [0, 1]}, 500);
+//   });
+// }
+thumbImages.forEach((thumbImage) => {
+  thumbImage.addEventListener('mouseover', (event) => {
+    mainImage.src = event.target.src;
+    mainImage.animate({opacity: [0, 1]}, 500);
+  });
+});
